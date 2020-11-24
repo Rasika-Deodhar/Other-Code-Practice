e7458d2176ecda9a1f89a7b7fdf2edcf4882ff1e
Rasika Deodhar <rasikadeodhar95@gmail.com>
2020-11-24 10:37:28 +0000

    Old course and project revisited - formatting done, course content added

--- Stopwatch App/Stopwatch_node/app.js
+++ Stopwatch App/Stopwatch_node/app.js
@@ -16,12 +16,12 @@ app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -33,7 +33,7 @@ app.use(function(err, req, res, next) {
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mean-angular6', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection successful'))
+  .then(() => console.log('connection successful'))
   .catch((err) => console.error(err));
 
 module.exports = app;
