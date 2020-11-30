42b24bb418bdc731b82ac416b929a22357b5d79d
Rasika Deodhar <rasikadeodhar95@gmail.com>
2020-11-30 23:58:56 +0000

    Start Stop done in UI

--- Stopwatch App/Stopwatch/src/app/stopwatch/stopwatch.component.ts
+++ Stopwatch App/Stopwatch/src/app/stopwatch/stopwatch.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
@@ -8,13 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class StopwatchComponent implements OnInit {
 
   start_clock = "";
+  startDate = new Date('2020-03-08 00:00:00');
+  time_lapse: any;
+  myDate: any;
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  start(){
+  start() {
     this.start_clock = "Hello";
+    this.myDate = timer(1000, 1000)
+      .pipe(
+        map((x: number) => {
+          const newDate = new Date(this.startDate.getTime());
+          newDate.setSeconds(newDate.getSeconds() + x);
+          return newDate;
+        })
+      ).subscribe(t => this.time_lapse = t);
+
+  }
+
+  stop() {
+    this.myDate.unsubscribe();
   }
 
 }
